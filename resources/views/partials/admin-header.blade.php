f1a33f6c653fb2d95c16ada470e6735a55777322
David Makanjuola <21433dm@gmail.com>
2018-11-19 13:12:23 +0000

    Add files via upload

---
+++ resources/views/partials/admin-header.blade.php
@@ -0,0 +1,6 @@
+<div class="container-fluid">
+    <nav class="nav">
+        <a href="{{ route('blog.index') }}" class="nav-link">Laravel Guide</a>
+        <a href="{{ route('admin.index') }}" class="nav-link">Posts</a>
+    </nav>
+</div>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<div class="container-fluid">
+    <nav class="nav">
+        <a href="{{ route('blog.index') }}" class="nav-link">Laravel Tutorial</a>
+        <a href="{{ route('blog.index') }}" class="nav-link">Blog</a>
+        <a href="{{ route('other.about') }}" class="nav-link">About</a>
+    </nav>
+</div>
